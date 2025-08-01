dc14984c55e8cbeec30ff4853aca3bc2453ae441
Zeeshan Ashraf <zeshig01@gmail.com>
2025-08-01 08:25:40 -0700

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 import Header from "@/components/Header";
 import Image from "next/image";
+import Banner from "./home/Banner";
+import About from "./home/About";
+import Section from "./home/Section";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-  <Header/>
+  // <Header/>
+  <>
+        <Header/>
+        <Banner/>
+        <About/>
+        <Section/>
+        <Footer/>
+    </>
   );
 }
